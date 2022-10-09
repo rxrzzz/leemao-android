59b10df723ebe168994a7e404bc997d2d3c07185
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-10-09 15:16:27 +0100

    eslint and prettier setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
   extends: [
     "eslint:recommended",
@@ -19,6 +20,8 @@ module.exports = {
     },
   },
   rules: {
-
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx"] }], //should add ".ts" if typescript project
   },
 };
