f692d31160ae07bc25c67261161841cca243ba63
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-10-11 10:24:54 +0100

    added custom grid

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,19 @@ import { SectionTwo } from "./features/landing/SectionTwo";
 
 function App() {
   return (
-    <div className="w-10/12 mx-auto">
-      <Navbar />
-      <div className="w-full lg:w-1/2">
-        <SectionOne />
-        <SectionTwo />
-        <SectionThree />
-        <SectionFour />
+    <div className="w-full">
+      <div className="flex">
+        <div className="w-full lg:w-7/12 mx-6">
+
+          <Navbar />
+          <SectionOne />
+          <SectionTwo />
+          <SectionThree />
+          <SectionFour />
+        </div>
+        <div className="hidden lg:block w-5/12">
+          <img src="/grid1.svg" alt="#" className="sticky top-0 right-0 w-full" />
+        </div>
       </div>
       <Footer />
     </div>
